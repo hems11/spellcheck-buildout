08dcf9ba09ce323b77c52178cc037ff74907d96c
hemali1110 <hemalivelani11@gmail.com>
2025-11-01 20:47:44 +0530

    spell check implimentation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SpellCheck from './components/SpellCheck';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellCheck/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SpellCheck.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const customDictionary = {
+        teh: "the",
+        wrok: "work",
+        fot: "for",
+        exampl: "example"
+    };
+
+const SpellCheck = () => {
+   const [inputText, setInputText] = useState("");
+   const [suggestion, setSuggestion] = useState("");
+   
+   const handleInputChange = (e) => {
+    const text = e.target.value;
+    const words = text.split(" ");
+
+    const correctedWords = words.map((word) => customDictionary[word.toLowerCase()] || word);
+    const firstWrong = words.find((word, i) => correctedWords[i] !== word && word.trim() !== "")
+
+    setInputText(text);
+    setSuggestion(
+        firstWrong ? `${firstWrong} -> ${customDictionary[firstWrong.toLowerCase()]}` : ""
+    );
+
+   }
+
+    return (
+        <div>
+            <h1>Spell Check and Auto-Correction </h1>
+            <textarea
+            value={inputText}
+            onChange={handleInputChange}
+            rows={5}
+            cols={50} 
+            placeholder="Enter text..."
+            />
+
+            {suggestion && (
+                <p> Did you mean: <strong>{suggestion}</strong></p>
+            )}
+        </div>
+    )
+
+}
+
+export default SpellCheck;
